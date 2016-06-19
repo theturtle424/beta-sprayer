617c503866228213469b17cefa0302c36e38b175
Franklin DeHart <franklintheturtle@gmail.com>
2016-06-19 16:56:23 -0400

    New xcode project

---
+++ Beta Sprayer 5000/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beta Sprayer 5000
+//
+//  Created by Franklin DeHart on 6/19/16.
+//  Copyright © 2016 Franklin DeHart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
